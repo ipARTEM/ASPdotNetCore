b470726322a195771f4a8a32b10a92863491e41c
Artem <a.khimin@yandex.ru>
2021-05-31 15:24:14 +0300

    Создал  View и Controller

--- ASPdotNetCore/Startup.cs
+++ ASPdotNetCore/Startup.cs
@@ -12,13 +12,17 @@ namespace ASPdotNetCore
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+       
         public void ConfigureServices(IServiceCollection services)
         {
+            // добавляем поддержку контроллеров и представлений(MVC)
+            services.AddControllersWithViews()
+                // выставляем совместимость с asp.net core 3.0
+                .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -28,12 +32,14 @@ namespace ASPdotNetCore
 
             app.UseRouting();
 
+            //подключаем поддержку статичных файлов в приложении (css, js и т.д.)
+            app.UseStaticFiles();
+
+            //регистрируем нужные нам маршруты 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                 endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+                
             });
         }
     }
